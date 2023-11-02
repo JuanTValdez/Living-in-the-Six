c980c248a3c6e64c813088bef3908868e9444339
Juan Valdez <juantvaldez85@gmail.com>
2023-11-02 19:22:20 -0400

    start sign up component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Login from './components/Login';
+// import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
 	return (
 		<div className='App'>
-			<Login />
+			{/* <Login />
+			 */}
+
+			<SignUp />
 		</div>
 	);
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import '../styles/SignUp.css';
 
 export default function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+	return (
+		<div className='sign-up-container'>
+			<div className='media-sign-up'>
+				<div className='member'>Become a Member</div>
+			</div>
+		</div>
+	);
 }
