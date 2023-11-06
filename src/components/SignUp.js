20e1ad3e529373a43063dd262df167e21a5a68bb
Juan Valdez <juantvaldez85@gmail.com>
2023-11-06 16:53:35 -0500

    finished style close btn in signup modal

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/SignUp.css';
+import Close from '../images/times-circle-solid.svg';
 
 export default function SignUp() {
 	return (
 		<div className='sign-up-container'>
 			<div className='media-sign-up'>
-				<div className='member'>Become a Member<div className="close-btn"></div></div>
+				<div className='member'>
+					Become a Member<img className='close-btn' src={Close}></img>
+				</div>
 			</div>
 		</div>
 	);
