3a4b653aedbf87f53917662e31372244f33c045d
Juan Valdez <juantvaldez85@gmail.com>
2023-11-06 22:47:31 -0500

    added google GIS oauth and sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SignUp from './components/SignUp';
 
 function App() {
+
+	
 	return (
 		<div className='App'>
 			{/* <Login />
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import { useEffect, React } from 'react';
 import '../styles/SignUp.css';
+
 import Close from '../images/times-circle-solid.svg';
 
+const { jwtDecode } = require('jwt-decode');
 export default function SignUp() {
+	// const handleCallbackResponse = function (response) {
+	// 	var userObject = jwt_decode(response.credential);
+	// 	console.log(userObject);
+	// };
+
+	function handleCallbackResponse(response) {
+		var userObject = jwtDecode(response.credential);
+		console.log(userObject);
+	}
+
+	useEffect(() => {
+		/* global google */
+		google.accounts.id.initialize({
+			client_id:
+				'506919281725-6elvtkqshqv70g70vr00e2ctc04rm2j8.apps.googleusercontent.com',
+			callback: handleCallbackResponse,
+		});
+
+		google.accounts.id.renderButton(document.getElementById('google-sign-in'), {
+			theme: 'outline',
+			size: 'large',
+		});
+	}, []);
 	return (
 		<div className='sign-up-container'>
 			<div className='media-sign-up'>
 				<div className='member'>
-					Become a Member<img className='close-btn' src={Close}></img>
+					Become a Member
+					<img className='close-btn' src={Close} alt='Close'></img>
 				</div>
+
+				<div id='google-sign-in'></div>
 			</div>
 		</div>
 	);
