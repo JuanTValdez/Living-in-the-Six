2e9172061f6e47c347125c6138f32c3837a27489
Juan Valdez <juantvaldez85@gmail.com>
2023-11-15 21:57:44 -0500

    styled logo - not complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,20 @@ import '../styles/NavBar.css';
 
 export default function NavBar() {
 	return (
-		<nav className="nav">
-			<div className='logo'>Living in the 6ix</div>
-			<ul className="font-size-30px">
-				<li className='login-btn'>Login</li>
-				<li className="signup-btn">Signup</li>
-			</ul>
-		</nav>
+		<div>
+			<nav className='nav'>
+				<div className='logo'>
+					<p className='font-size-16'>LIVING</p>
+					<p className='font-size-16'>IN THE</p>
+					<p className='font-size-35 six'>6IX</p>
+				</div>
+				<ul className='font-size-30'>
+					<li className='login-btn'>Login</li>
+					<li className='signup-btn'>Signup</li>
+				</ul>
+			</nav>
+
+      <div className="test-block"></div>
+		</div>
 	);
 }
