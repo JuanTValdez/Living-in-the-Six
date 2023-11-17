01a7a89c142fa6a3930ba6a125ed8a0f3f482ba5
Juan Valdez <juantvaldez85@gmail.com>
2023-11-16 22:43:03 -0500

    adding form to signup

--- src/components/FacebookAuth.js
+++ src/components/FacebookAuth.js
@@ -5,6 +5,11 @@ import { FacebookLoginButton } from 'react-social-login-buttons';
 export default function FacebookAuth() {
 	const [user, setUser] = useState({});
 
+	const FACEBOOK_STYLES = {
+		fontSize: '4rem',
+		width: '260px',
+	};
+
 	const handleFacebookResponse = function (response) {
 		setUser(response.data);
 		console.log('Facebook Response: ', response.data);
@@ -20,7 +25,11 @@ export default function FacebookAuth() {
 					onReject={(error) => {
 						console.log(error);
 					}}>
-					<FacebookLoginButton text='Continue with Facebook' />
+					<FacebookLoginButton
+						style={FACEBOOK_STYLES}
+						text='Continue with Facebook'
+						size='4rem'
+					/>
 				</LoginSocialFacebook>
 			</div>
 		</div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,15 +9,24 @@ import Close from '../images/times-circle-solid.svg';
 export default function SignUp() {
 	return (
 		<div className='sign-up-container'>
-			<div className='header font-size-22'>
-				<p className='member'>Become a Member</p>
+			<div className='header '>
+				<p className='member font-size-22'>Become a Member</p>
 				<img className='close-btn' src={Close} alt=''></img>
 			</div>
-			
+
 			<div className='media-sign-up'>
 				<GoogleAuth />
 				<FacebookAuth />
 			</div>
+
+			<div className='underline'></div>
+			<form className='sign-up-form'>
+				<input type='text' />
+				<input type='text' />
+				<input type='text' />
+				<input type='text' />
+				<input type='text' />
+			</form>
 		</div>
 	);
 }
