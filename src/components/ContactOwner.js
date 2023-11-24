81edd4491285c8371ceb043648f7961fe358bfd1
Juan Valdez <juantvaldez85@gmail.com>
2023-11-24 17:19:32 -0500

    add form to contact owner component

--- src/components/ContactOwner.js
+++ src/components/ContactOwner.js
@@ -2,9 +2,19 @@ import React from 'react';
 import '../styles/ContactOwner.css';
 
 export default function ContactOwner() {
-	return <div className='contact-form'>
-
-  <h1 className="owner-title">Contact Owner</h1>
-
-  </div>;
+	return (
+		<div className='contact-form'>
+			<h1 className='owner-title font-size-22'>Contact Owner</h1>
+			<form action=''>
+				<input className='contact-input' placeholder='Name * ' type='text' />
+				<input className='contact-input' placeholder='Phone *' type='text' />
+				<input className='contact-input' placeholder='Email *' type='text' />
+				<input
+					className='contact-input lrg-input'
+					placeholder="Hi, i'd like to schedule a visit to your rental property. Please let me know when the best date and time would be. Thank you!"
+					type='text'
+				/>
+			</form>
+		</div>
+	);
 }
