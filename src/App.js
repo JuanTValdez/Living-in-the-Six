47b8afb9c18f6db494bab3f9bb7c18e2252b9b9d
Juan Valdez <juantvaldez85@gmail.com>
2023-11-24 16:48:31 -0500

    created contactowner component and stylesheet

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './GlobalVariables.css';
 // import Login from './components/Login';
 import SignUp from './components/SignUp';
 // import Home from './components/Home.js';
-
+import ContactOwner from './components/ContactOwner.js';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* <Login /> */}
-			<SignUp />
-
+			{/* <SignUp /> */}
 			{/* <Home /> */}
-			
+
+			<ContactOwner />
 		</div>
 	);
 }
---
+++ src/components/ContactOwner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/ContactOwner.css';
+
+export default function ContactOwner() {
+	return <div className='contact-form'>
+
+  <h1 className="owner-title">Contact Owner</h1>
+
+  </div>;
+}
