dbf857697235c4f755f04038e9cea912ee0372aa
Juan Valdez <juantvaldez85@gmail.com>
2023-11-30 22:37:17 -0500

    final save - working on home screen select elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,25 @@ export default function Home() {
 		<>
 			<NavBar />
 			<div className='gradient'>
-				<div className='wrapper'>
+				<div className='container'>
 					<h1>Find your way home</h1>
 					<p>
 						Looking for a new apartment in Toronto can be a hassle, but using
 						our website can make your search a lot easier and faster!
 					</p>
 					<form className='flex-col'>
-						<input type='dropdown' />
-						<input type='dropdown' />
-						<input type='dropdown' />
+						<select value='hoods' required>
+							<option value='' disabled selected hidden>
+								Select your hood
+							</option>
+							<option value=''>My Hood</option>
+							<option value=''>Your Hood</option>
+							<option value=''>Their Hood</option>
+						</select>
+						<select type='dropdown' />
+						<select type='dropdown' />
 						<button>Search</button>
-					</form>{' '}
+					</form>
 				</div>
 			</div>
 		</>
