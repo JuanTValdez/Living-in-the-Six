565960684d03d44c820245bdfa0ae751adfb3793
Juan Valdez <juantvaldez85@gmail.com>
2023-12-01 19:20:05 -0500

    styling home page form

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,14 @@ export default function Home() {
 		<>
 			<NavBar />
 			<div className='gradient'>
-				<div className='container'>
-					<h1>Find your way home</h1>
-					<p>
+				<div className='form-container'>
+					<h1 className='font-size-60'>Find your way home</h1>
+					<p classname='font-size-16'>
 						Looking for a new apartment in Toronto can be a hassle, but using
 						our website can make your search a lot easier and faster!
 					</p>
-					<form className='flex-col'>
-						<select value='hoods' required>
+					<form className='flex-col homepage-form'>
+						<select className='dropdown' value='hoods' required>
 							<option value='' disabled selected hidden>
 								Select your hood
 							</option>
@@ -22,8 +22,8 @@ export default function Home() {
 							<option value=''>Your Hood</option>
 							<option value=''>Their Hood</option>
 						</select>
-						<select type='dropdown' />
-						<select type='dropdown' />
+						<select className='dropdown' type='dropdown' />
+						<select className='dropdown' type='dropdown' />
 						<button>Search</button>
 					</form>
 				</div>
