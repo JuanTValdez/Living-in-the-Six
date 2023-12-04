14c66411bbb648430c542e59aefff7f419debff4
Juan Valdez <juantvaldez85@gmail.com>
2023-12-04 17:43:05 -0500

    finished home page form layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,30 +9,46 @@ export default function Home() {
 			<div className='gradient'>
 				<div className='form-container'>
 					<h1 className='font-size-60'>Find your way home</h1>
-					<p classname='font-size-16'>
+					<p className='font-size-16'>
 						Looking for a new apartment in Toronto can be a hassle, but using
 						our website can make your search a lot easier and faster!
 					</p>
 					<form className='flex-col homepage-form'>
-						<select className='dropdown' value='hoods' required>
-							<option value='' disabled selected hidden>
+						<select className='dropdown font-size-13' value='hoods' required>
+							<option value='change' disabled selected hidden>
 								Select your hood
 							</option>
-							<option value=''>My Hood</option>
-							<option value=''>Your Hood</option>
-							<option value=''>Their Hood</option>
+							<option className='font-size-13' value=''>
+								My 'Hood'
+							</option>
+							<option className='font-size-13' value=''>
+								Your Hood
+							</option>
+							<option className='font-size-13' value=''>
+								Their Hood
+							</option>
 						</select>
-						<select className='dropdown' type='dropdown'>
-							<option value='' disabled selected hidden>
-								Select your hood
+						<select className='dropdown font-size-13' type='dropdown'>
+							<option
+								className='font-size-13'
+								value='change'
+								disabled
+								selected
+								hidden>
+								Price
 							</option>
 						</select>{' '}
-						<select className='dropdown' type='dropdown'>
-							<option value='' disabled selected hidden>
-								Select your hood
+						<select className='dropdown font-size-13' type='dropdown'>
+							<option
+								className='font-size-13'
+								value='change'
+								disabled
+								selected
+								hidden>
+								Number of bedrooms
 							</option>
 						</select>
-						<button>Search</button>
+						<button className='iris-btn font-size-10'>Search</button>
 					</form>
 				</div>
 			</div>
