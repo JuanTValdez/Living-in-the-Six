5d42b1aaf202896da16fa3feb8df91fad3ddb9f9
Juan Valdez <juantvaldez85@gmail.com>
2023-12-08 17:28:44 -0500

    completed footer layout. Added media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import '../styles/Footer.css';
+import Twitter from '../images/twitter_sml.svg';
+import Facebook from '../images/facebook_sml.svg';
+import Insta from '../images/instagram_icon.svg';
 
 export default function Footer() {
 	return (
 		<div className='footer-container'>
 			<div className='stay-connected'>
-				<h5> Stay Connected</h5>
-				<p>Get social and stay up to date with all the newest listings!</p>
+				<h5 className='font-size-22'> Stay Connected</h5>
+				<p className='font-size-14'>
+					Get social and stay up to date with all the newest listings!
+				</p>
+				<div className='media-links'>
+					<img src={Twitter} alt='' />
+					<img src={Facebook} alt='' />
+					<img src={Insta} alt='' />
+				</div>
 			</div>{' '}
 			<div className='subscribe'>
-				<h5>Subscribe</h5>
-				<p>
-					Receive tips, neighbourhood guides, and general information to renting
+				<h5 className='font-size-22'>Subscribe</h5>
+				<p className='font-size-14'>
+					Receive tips, neighborhood guides, and general information to renting
 					in the city.
 				</p>
+				<form action='' className=''>
+					<input type='text' />
+					<button>Subscribe</button>
+				</form>
+				<div className='contact'>
+					<p>Contact</p>
+					<p>Terms</p>
+					<p>Private policy</p>
+				</div>
 			</div>
 		</div>
 	);
