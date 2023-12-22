56561182b76655e034535fbfd6d301d43a0e0ef6
Juan Valdez <juantvaldez85@gmail.com>
2023-12-22 16:30:46 -0500

    added logo img to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../styles/NavBar.css';
+import Logo from '../images/logo.svg';
 
 export default function NavBar() {
 	return (
 		<div>
 			<nav className='nav'>
 				<div className='logo'>
+					<img src={Logo} alt='hh' />
+				</div>
+				{/* <div className='logo'>
 					<p className='font-size-16'>LIVING</p>
 					<p className='font-size-16'>IN THE</p>
 					<p className='font-size-35 six'>6IX</p>
-				</div>
+				</div> */}
 				<ul className='font-size-30'>
 					<li>
 						<button className='dusk-btn'>Log in</button>
@@ -19,8 +23,6 @@ export default function NavBar() {
 					</li>
 				</ul>
 			</nav>
-
-			<div className='test-block'></div>
 		</div>
 	);
 }
