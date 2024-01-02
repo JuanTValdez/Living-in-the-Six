f7142f29ed145114f940fa9823a5423d39205e6a
Juan Valdez <juantvaldez85@gmail.com>
2024-01-02 17:10:49 -0500

    added drop down input for home requirements

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -5,14 +5,20 @@ export default function AccountSettings() {
 	return (
 		<div className='settings-container'>
 			<form action=''>
-				<select name='' id=''>
-					<option className= value=''>Select your 'hood'</option>
+				<select className='dropdown font-size-13' name='' id=''>
+					<option className='font-size-13' value=''>
+						Select your 'hood'
+					</option>
 				</select>
-				<select name='' id=''>
-					<option value=''>Price range</option>
+				<select className='dropdown font-size-13' name='' id=''>
+					<option className='font-size-13' value=''>
+						Price range
+					</option>
 				</select>
-				<select name='' id=''>
-					<option value=''>Number of Bedrooms</option>
+				<select className='dropdown font-size-13' name='' id=''>
+					<option className='font-size-13' value=''>
+						Number of Bedrooms
+					</option>
 				</select>
 				<button className='iris-btn font-size-10'>Search</button>
 			</form>
