4654f922cf22f05fba09b24954a83d672069bd22
Juan Valdez <juantvaldez85@gmail.com>
2024-01-02 17:26:07 -0500

    add personal information section to settings comp

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -22,6 +22,19 @@ export default function AccountSettings() {
 				</select>
 				<button className='iris-btn font-size-10'>Search</button>
 			</form>
+			<h6>Account</h6>
+			<h6>|</h6>
+			<h6>Saved Listings</h6>
+			<h1>Account settings</h1>
+			<section>
+				<h4>Personal Information</h4>
+				<label htmlFor=''>Name</label>
+				<input placeholder='John Smith' type='text' />
+				<label htmlFor=''>Phone</label>
+				<input placeholder='555-1018' type='text' />
+				<label htmlFor=''></label>
+				<input placeholder='jonh.smith@gmail.com' type='text' />
+			</section>
 		</div>
 	);
 }
