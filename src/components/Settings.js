fa45ee3e6f923c2f18095f75ad260a636bdaa253
Juan Valdez <juantvaldez85@gmail.com>
2024-01-02 19:51:08 -0500

    configuring settings page layout

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -4,7 +4,7 @@ import '../styles/Settings.css';
 export default function AccountSettings() {
 	return (
 		<div className='settings-container'>
-			<form action=''>
+			<form className='dropdown-settings' action=''>
 				<select className='dropdown font-size-13' name='' id=''>
 					<option className='font-size-13' value=''>
 						Select your 'hood'
@@ -22,18 +22,29 @@ export default function AccountSettings() {
 				</select>
 				<button className='iris-btn font-size-10'>Search</button>
 			</form>
-			<h6>Account</h6>
-			<h6>|</h6>
-			<h6>Saved Listings</h6>
-			<h1>Account settings</h1>
-			<section>
-				<h4>Personal Information</h4>
-				<label htmlFor='name'>Name</label>
-				<input placeholder='John Smith' type='text' id='name' value='name' />
-				<label htmlFor=''>Phone</label>
-				<input placeholder='555-1018' type='text' />
-				<label htmlFor=''></label>
-				<input placeholder='jonh.smith@gmail.com' type='text' />
+
+			<div className='flex-col'>
+				<p>Account</p>
+				<p>|</p>
+				<p>Saved Listings</p>
+			</div>
+
+			{/*<h1>Account settings</h1>
+				{/*<section>
+				<div>
+					<h4>Personal Information</h4>
+					<label htmlFor='name'>Name</label>
+					<input placeholder='John Smith' type='text' id='name' value='name' />
+				</div>
+				<div>
+					<label htmlFor=''>Phone</label>
+					<input placeholder='555-1018' type='text' />
+				</div>
+				<div>
+					{' '}
+					<label htmlFor=''>Email</label>
+					<input placeholder='john.smith@gmail.com' type='text' />
+				</div>
 			</section>
 
 			<section>
@@ -44,7 +55,7 @@ export default function AccountSettings() {
 				<input type='text' />
 				<label htmlFor=''>Repeat New Password</label>
 				<input type='text' />
-			</section>
+			</section> */}
 		</div>
 	);
 }
