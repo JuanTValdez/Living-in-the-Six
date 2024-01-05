79a7df38d7fcc21e64fd4eaa2774b398dff48507
Juan Valdez <juantvaldez85@gmail.com>
2024-01-04 22:46:48 -0500

    continue layout/style of ListCard comp

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -7,25 +7,36 @@ import SaveListingOn from '../images/save-listing-on.svg';
 import SaveListingOff from '../images/save-listing-off.svg';
 export default function ListingCard() {
 	return (
-		<div className='card-wrapper'>
-			<img src={ListingImage} alt='' className='listing-img' />
-			<div className='posted-wrapper'>
-				<p className='font-size-10'>Posted 1 hour ago</p>
-				<div className='listing-icons'>
-					<img src={PawPrint} alt='' />
-					<img src={Parking} alt='' />
-				</div>
-			</div>
-
-			<div className='price-wrapper'>
-				<div className='price-address'>
-					<p className='price font-size-22'>$1597</p>
-					<p className='address font-size-14'>70 Spadina Rd.</p>
+		<>
+			<h1 className='neighborhood-name font-size-22'>Liberty Village</h1>
+			<div className='card-wrapper'>
+				<img src={ListingImage} alt='' className='listing-img' />
+				<div className='posted-wrapper'>
+					<p className='font-size-10'>Posted 1 hour ago</p>
+					<div className='listing-icons'>
+						<img src={PawPrint} alt='' />
+						<img src={Parking} alt='' />
+					</div>
 				</div>
 
-				<img src={SaveListingOn} alt='' className='save-listing' />
-				{/* <img src={SaveListingOff} alt='' className='save-listing' /> */}
+				<div className='price-wrapper'>
+					<div className='price-address'>
+						<p className='price font-size-22'>$1597</p>
+						<p className='address font-size-14'>70 Spadina Rd.</p>
+					</div>
+
+					<img src={SaveListingOn} alt='' className='save-listing' />
+					{/* <img src={SaveListingOff} alt='' className='save-listing' /> */}
+				</div>
+				<div className='listing-details'>
+					{' '}
+					<p className='item1 font-size-10'>2 Bed</p>
+					<p className='item2 font-size-10'>1 Bath </p>
+					<p className='item1 font-size-10'> 850 sq/ft </p>
+					<p className='item2 font-size-10'>Apartment</p>{' '}
+					<button className='iris-btn font-size-10'>VIEW DETAILS</button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
