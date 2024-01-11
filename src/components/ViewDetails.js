ec10369b9fa502ce19e5102e8adb86f51bac587f
Juan Valdez <juantvaldez85@gmail.com>
2024-01-11 16:57:24 -0500

    continue viewdetails layout

--- src/components/ViewDetails.js
+++ src/components/ViewDetails.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../styles/ViewDetails.css';
 import ListingImg from '../images/listing-img.png';
+import Garbage from '../images/icon-garbage.svg';
+import Heating from '../images/icon-heating.svg';
+import Recycling from '../images/icon-recycling.svg';
+import Water from '../images/icon-water.svg';
 
 export default function ViewDetails() {
 	return (
@@ -14,6 +18,30 @@ export default function ViewDetails() {
 				<div className='tabs'>
 					<div className='photos-tab font-size-10'>PHOTOS</div>
 					<div className='map-tab font-size-10'>MAP</div>
+				</div>
+
+				<div className='utilities'>
+					<div className='included'>Utilities Included</div>
+					<div className='utils-listed'>
+						<div>
+							<img src={Garbage} alt='' />
+
+							<p>Garbage</p>
+						</div>
+						<div>
+							<img src={Heating} alt='' />
+							<p>Heating</p>
+						</div>
+						<div>
+							<img src={Recycling} alt='' />
+							<p>Recycling</p>
+						</div>
+
+						<div>
+							<img src={Water} alt='' />
+							<p>Water</p>
+						</div>
+					</div>
 				</div>
 			</div>
 			ViewDetails
