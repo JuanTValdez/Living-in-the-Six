6e2dad6aad45484fec45f3894afae8f7889f996c
Juan Valdez <juantvaldez85@gmail.com>
2024-01-10 21:46:10 -0500

    saving - work on details layout

--- src/components/ViewDetails.js
+++ src/components/ViewDetails.js
@@ -7,7 +7,9 @@ export default function ViewDetails() {
 		<div>
 			<div className='details-container'>
 				<div className='listing-img'>
+					{/* img is 5 layers thick */}
 					<img src={ListingImg} alt=''></img>
+					{/* Layer below img is 2 rows thicks */}
 				</div>
 			</div>
 			ViewDetails
