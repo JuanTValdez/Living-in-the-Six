469b8c9fb858c665486c1f59919bd216b9ef5a27
Juan Valdez <juantvaldez85@gmail.com>
2024-01-25 19:05:46 -0500

    fixed width issue on signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -29,67 +29,74 @@ export default function SignUp({ onRef, isOpen, onClose }) {
 					<FacebookAuth />
 				</div>
 				<div className='underline'></div>
-				<p className='form-header font-size-18'>Continue with Email</p>
-				<form className='sign-up-form' method='GET'>
-					<input className='input-style' placeholder='Name *' type='text' />
-					<input className='input-style' placeholder='Phone *' type='text' />
-					<input
-						className='input-style full-length'
-						placeholder='Email *'
-						type='text'
-					/>
-					<input className='input-style' placeholder='Password *' type='text' />
-					<input
-						className='input-style'
-						placeholder='Repeat Password *'
-						type='text'
-					/>
+				<div className='sign-up-wrapper'>
+					<p className='form-header font-size-18'>Continue with Email</p>
+					<form className='sign-up-form' method='GET'>
+						<input className='input-style' placeholder='Name *' type='text' />
+						<input className='input-style' placeholder='Phone *' type='text' />
+						<input
+							className='input-style full-length'
+							placeholder='Email *'
+							type='text'
+						/>
+						<input
+							className='input-style'
+							placeholder='Password *'
+							type='text'
+						/>
+						<input
+							className='input-style'
+							placeholder='Repeat Password *'
+							type='text'
+						/>
 
-					<h5 className='required full-length font-size-10 align-right'>
-						* required fields
-					</h5>
+						<h5 className='required full-length font-size-10 align-right'>
+							* required fields
+						</h5>
 
-					<label className='required font-size-14' htmlFor=''>
-						I'm a*
-					</label>
-
-					<div className='align-item-baseline'>
-						<input type='radio' name='tenant-type' />
-						<label
-							className='radio-label required font-size-14 '
-							htmlFor='Renter'>
-							Renter
+						<label className='required font-size-14' htmlFor=''>
+							I'm a*
 						</label>
 
-						<input type='radio' name='tenant-type' />
-						<label
-							className='radio-label required font-size-14'
-							htmlFor='Renter'>
-							Landlord
-						</label>
-					</div>
-				</form>
+						<div className='align-item-baseline'>
+							<input type='radio' name='tenant-type' />
+							<label
+								className='radio-label required font-size-14 '
+								htmlFor='Renter'>
+								Renter
+							</label>
 
-				<div className='have-account'>
-					<div>
-						<span className='required font-size-14'>
-							Already have an account?
-						</span>{' '}
-						<a
-							style={{
-								fontWeight: 'bold',
-								color: '#6363b7',
-								letterSpacing: '2px',
-								lineHeight: '3.6px',
-							}}
-							className='gotham-book font-size-10'
-							href='localhost:3000'>
-							LOGIN
-						</a>{' '}
+							<input type='radio' name='tenant-type' />
+							<label
+								className='radio-label required font-size-14'
+								htmlFor='Renter'>
+								Landlord
+							</label>
+						</div>
+					</form>
+
+					<div className='have-account'>
+						<div>
+							<span className='required font-size-14'>
+								Already have an account?
+							</span>{' '}
+							<a
+								style={{
+									fontWeight: 'bold',
+									color: '#6363b7',
+									letterSpacing: '2px',
+									lineHeight: '3.6px',
+									marginLeft: '1rem',
+								}}
+								className='gotham-book font-size-10'
+								href='localhost:3000'>
+								LOGIN
+							</a>{' '}
+						</div>
+						<button className='register gotham-book font-size-10'>
+							REGISTER
+						</button>
 					</div>
-					<button className='register gotham-book font-size-10'>
-						REGISTER
-					</button>
 				</div>
 			</div>
 		</dialog>
